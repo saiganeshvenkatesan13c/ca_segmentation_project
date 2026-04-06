5a3f4e33164d236597a03b15e93589b80e26ed8d
saiganeshvenkatesan13c <saiganeshvenkatesan13c@gmail.com>
2026-04-06 19:36:00 +0200

    Update compute_metrics.py

--- src/model_evaluation/compute_metrics.py
+++ src/model_evaluation/compute_metrics.py
@@ -1,3 +1,38 @@
 """
 Compute segmentation metrics (F1, Precision, Recall).
 """
+
+import numpy as np
+import pandas as pd
+from stardist.matching import matching
+from tqdm import tqdm
+
+input_file = "/home/qbx911/datadir/cellpose_results.pkl"
+df = pd.read_pickle(input_file)
+
+f1_scores = [] # harmonic mean of precision and recall
+precisions = [] # how many of the predicted positives are actually correct
+recalls = [] # how many of the actual positives were correctly predicted.
+mean_true_scores = [] # average IoU of all correctly matched ground‑truth objects
+
+for row in tqdm(df.itertuples(index=False), total=len(df)):
+    pred_mask = row.pred_mask.astype(np.int32)
+    gt_mask   = row.gt_mask.astype(np.int32)
+
+    match = matching(
+        gt_mask,
+        pred_mask,
+        thresh=0.5, #IoU threshold for a match is 50%
+        report_matches=False
+    )
+
+    f1_scores.append(match.f1)
+    precisions.append(match.precision)
+    recalls.append(match.recall)
+    mean_true_scores.append(match.mean_true_score)
+
+df["f1_score"] = f1_scores
+df["precision"] = precisions
+df["recall"] = recalls
+df["mean_true_score"] = mean_true_scores
+``
